46478bd0af03de98464f95075ae7435c0ef90796
faisandelviento <cristinaclaverluna@hotmail.com>
2024-04-01 22:56:22 +0200

    navbar sin estilar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "./ui/footer";
+import NavBar from "./ui/navbar";
 
 export const metadata: Metadata = {
   title: "KukuMarket",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NavBar/>
         {children}
         <Footer/>
       </body>
